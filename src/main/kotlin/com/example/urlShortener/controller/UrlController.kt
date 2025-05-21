e56b80f3ce9897790c918d57e3c71a847a118ebc
anujachakraborty <anujac234@gmail.com>
2025-05-21 21:45:41 +0200

    Added basic logging to shorten and resolve the URL

--- src/main/kotlin/com/example/urlShortener/controller/UrlController.kt
+++ src/main/kotlin/com/example/urlShortener/controller/UrlController.kt
@@ -14,22 +14,27 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.slf4j.LoggerFactory
 
 @RestController
 @RequestMapping("/api")
 @Validated
 class UrlController(@Autowired val urlService: UrlService) {
 
+    val logger = LoggerFactory.getLogger(UrlController::class.java)
+
     @PostMapping("/shorten")
     fun shortenUrl(@Valid @RequestBody urlRequest: UrlRequest): ResponseEntity<String> {
         //val url = body["url"] ?: return ResponseEntity.badRequest().body("Missing URL")
         val hash = urlService.shortenUrl(urlRequest.url)
+        logger.info("Hash value of the corresponding url: $hash")
         return ResponseEntity.ok("https://short.ly/$hash")
     }
 
     @GetMapping("/{hash}")
     fun resolveUrl(@PathVariable hash: String): ResponseEntity<Any> {
         val url = urlService.getOriginalUrl(hash)
+        logger.debug("Fetching original URL for hash: $hash")
         return if (url != null)
             ResponseEntity.ok(mapOf("url" to url))
         else
--- src/main/kotlin/com/example/urlShortener/service/UrlService.kt
+++ src/main/kotlin/com/example/urlShortener/service/UrlService.kt
@@ -3,10 +3,12 @@ package com.example.urlShortener.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
-
+import org.slf4j.LoggerFactory
 
 @Service
 class UrlService(@Autowired val redisTemplate: StringRedisTemplate) {
+
+    val logger = LoggerFactory.getLogger(UrlService::class.java)
 
     private val valueOps = redisTemplate.opsForValue()
     private val counterKey = "url_counter"
@@ -14,6 +16,7 @@ class UrlService(@Autowired val redisTemplate: StringRedisTemplate) {
     fun shortenUrl(originalUrl: String): String {
         val id = redisTemplate.opsForValue().increment(counterKey) ?: 1L
         val shortId = encodeBase62(id)
+        logger.info("Shortened the URL: $originalUrl to base-62 encoded $shortId")
         valueOps.set(shortId, originalUrl)
         return shortId
     }
